08efc93bc05992451bf41ae0c1eb83afa358cdb8
Aditya Shinde <shinde.aditya386@gmail.com>
2022-04-30 06:13:18 -0400

    add neovim config

---
+++ nvim/lua/basic.lua
@@ -0,0 +1,35 @@
+local set = vim.opt
+
+-- Basic commands from .vim file
+
+set.showmatch = true
+set.compatible = false
+set.ignorecase = true
+set.hlsearch = true
+set.incsearch = true
+set.tabstop = 4
+set.softtabstop = 4
+set.expandtab = true
+set.shiftwidth = 4
+set.autoindent = true
+set.number = true
+set.cc = "80"
+set.cursorline = true
+set.ttyfast = true
+set.swapfile = false
+set.background = "dark"
+vim.cmd("syntax enable")
+
+-- Plugins and all
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin", "~/.nvim")
+-- Plug("dracula/vim")
+Plug("jacoborus/tender.vim")
+Plug("feline-nvim/feline.nvim")
+Plug("scrooloose/nerdtree")
+vim.call("plug#end")
+
+-- change colorscheme
+set.termguicolors = true
+vim.cmd("colorscheme tender")
+
