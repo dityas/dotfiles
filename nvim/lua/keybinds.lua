ede23d4192f1006113b8cc34e62ad6cdfd11fd98
Aditya Shinde <shinde.aditya386@gmail.com>
2022-06-23 18:30:37 -0400

    Update keybinds.lua

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -5,3 +5,4 @@ vim.api.nvim_set_keymap("", "<leader>q", ":NERDTreeClose<CR><LF>", {noremap = tr
 -- Buffer navigation
 vim.api.nvim_set_keymap("", "<C-p>", ":bprevious<CR><LF>", {noremap = true})
 vim.api.nvim_set_keymap("", "<C-n>", ":bnext<CR><LF>", {noremap = true})
+vim.api.nvim_set_keymap("", "<C-d>", ":bdelete<CR><LF>", {noremap = true})
